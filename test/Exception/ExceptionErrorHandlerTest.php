2e0d3e3d1a2435d0d8a624d3851d626c5e18ecfa
Michael Tsang <miklcct@gmail.com>
2018-11-15 17:00:24 +0800

    add ExceptionErrorHandlerTest

---
+++ test/Exception/ExceptionErrorHandlerTest.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Miklcct\ThinPhpApp\Test\Exception;
+
+use ErrorException;
+use Miklcct\ThinPhpApp\Exception\ExceptionErrorHandler;
+use PHPUnit\Framework\TestCase;
+
+class ExceptionErrorHandlerTest extends TestCase {
+
+    /** @var ErrorException */
+    private $exception;
+
+    /** @var int */
+    private $error_reporting;
+
+    protected function setUp() {
+        parent::setUp();
+        $this->exception = new ErrorException('test', 0, E_NOTICE);
+        $this->error_reporting = error_reporting();
+    }
+
+    protected function tearDown() {
+        error_reporting($this->error_reporting);
+        parent::tearDown();
+    }
+
+    public function testPositive() {
+        error_reporting($this->exception->getSeverity());
+        $this->expectExceptionObject($this->exception);
+        $handler = new ExceptionErrorHandler();
+        $handler(
+            $this->exception->getSeverity()
+            , $this->exception->getMessage()
+            , $this->exception->getFile()
+            , $this->exception->getLine()
+        );
+    }
+
+    public function testNegative() {
+        error_reporting(~$this->exception->getSeverity());
+        $this->expectNotToPerformAssertions();
+        $handler = new ExceptionErrorHandler();
+        $handler(
+            $this->exception->getSeverity()
+            , $this->exception->getMessage()
+            , $this->exception->getFile()
+            , $this->exception->getLine()
+        );
+    }
+}
