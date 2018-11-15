c7caa0e35ee49c028c433084901cae668e861014
Michael Tsang <miklcct@gmail.com>
2018-11-15 18:01:52 +0800

    complete ViewResponseFactoryTest

--- test/Response/ViewResponseFactoryTest.php
+++ test/Response/ViewResponseFactoryTest.php
@@ -28,4 +28,33 @@ class ViewResponseFactoryTest extends TestCase {
         $factory = new ViewResponseFactory($response_factory);
         self::assertSame($response, $factory($view));
     }
+
+    public function testWithContentType() {
+        $view = $this->createMock(View::class);
+        $stream = $this->createMock(StreamInterface::class);
+        $empty_response = $this->createMock(ResponseInterface::class);
+        $response_with_stream = $this->createMock(ResponseInterface::class);
+        $response_with_content_type = $this->createMock(ResponseInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+        $view->method('getContentType')->willReturn('text/plain');
+        $view->method('render')->willReturn($stream);
+        $response_factory = $this->createMock(ResponseFactoryInterface::class);
+        $response_factory->method('createResponse')
+            ->with(self::identicalTo(200))
+            ->willReturn($empty_response);
+        $empty_response->method('withBody')
+            ->with(self::identicalTo($stream))
+            ->willReturn($response_with_stream);
+        $response_with_content_type->method('withBody')
+            ->with(self::identicalTo($stream))
+            ->willReturn($response);
+        $empty_response->method('withHeader')
+            ->with(self::identicalTo('Content-Type'), self::identicalTo('text/plain'))
+            ->willReturn($response_with_content_type);
+        $response_with_stream->method('withHeader')
+            ->with(self::identicalTo('Content-Type'), self::identicalTo('text/plain'))
+            ->willReturn($response);
+        $factory = new ViewResponseFactory($response_factory);
+        self::assertSame($response, $factory($view));
+    }
 }
