7bf3d99a1892d547618d4fa414a35190e12f5af8
Michael Tsang <miklcct@gmail.com>
2020-02-24 20:36:07 +0800

    add failing test case for output buffering under exception

--- test/View/PhpTemplateTest.php
+++ test/View/PhpTemplateTest.php
@@ -3,9 +3,13 @@ declare(strict_types=1);
 
 namespace Miklcct\ThinPhpApp\Test\View;
 
+use ErrorException;
 use Miklcct\ThinPhpApp\View\PhpTemplate;
 use PHPUnit\Framework\TestCase;
+use Throwable;
 use Zend\Diactoros\StreamFactory;
+use function ob_get_contents;
+use function ob_get_level;
 
 class PhpTemplateTest extends TestCase {
     public function test__toString() : void {
@@ -20,5 +24,18 @@ class PhpTemplateTest extends TestCase {
         $view->method('getPathToTemplate')->willReturn(__DIR__ . '/test.phtml');
         $content = "Hello from " . __DIR__ . "/test.phtml";
         self::assertSame($content, $view->render()->__toString());
+    }
+
+    public function test__toStringDoesntLeakOutputBufferUnderException() {
+        $view = $this->getMockForAbstractClass(PhpTemplate::class, [new StreamFactory()]);
+        $view->method('getPathToTemplate')->willReturn(__DIR__ . '/error.phtml');
+        $level = ob_get_level();
+        $content = ob_get_contents();
+        try {
+            $view->__toString();
+        } catch (Throwable $exception) {
+        }
+        self::assertSame($level, ob_get_level());
+        self::assertSame($content, ob_get_contents());
     }
 }
